eb9ba78bda9ae32638ba681b65c3c214934223bc
srexrg <sreeragp276@gmail.com>
2024-11-30 03:27:06 +0530

    init

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,26 @@
+import {Text,StyleSheet,View } from "react-native"
+
+export default function AboutScreen(){
+
+    return (
+
+        <View style={styles.container}>
+
+            <Text style={styles.text}>About Screen</Text>
+
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
